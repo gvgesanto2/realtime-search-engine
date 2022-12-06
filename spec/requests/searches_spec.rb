7cfdc5c02ef041fe28796661cd580cc7215860bd
Gabriel Santo <gvgesanto2@gmail.com>
2022-12-06 03:18:20 -0300

    test: Add request specs for the GET /searches route

---
+++ spec/requests/searches_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Searches', type: :request do
+  describe 'GET /searches' do
+    before(:each) do
+      @fake_user = User.create(
+        name: 'Mr Test',
+        email: 'test@test.com',
+        password: '123123'
+      )
+      sign_in @fake_user
+      get '/searches' 
+    end
+
+    it 'returns an http success response' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template('searches/index')
+    end
+  end
+end
