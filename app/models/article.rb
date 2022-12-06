d155104b977b9991445533976596b20d8c022ff1
Gabriel Santo <gvgesanto2@gmail.com>
2022-12-06 00:51:49 -0300

    feat: Install the pg_trgm extension and improve the article search functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,23 @@
 class Article < ApplicationRecord
   include PgSearch::Model
+  belongs_to :user
   pg_search_scope :search_by_title, 
-                  against: :title,
+                  against: [
+                    [:title, 'A'],
+                    [:body, 'C'],
+                  ], 
+                  associated_against: {
+                    user: {:name => 'B'}
+                  },
                   using: {
-                    tsearch: { prefix: true, any_word: true }
+                    tsearch: { prefix: true, any_word: true },
+                    trigram: {
+                      only: [:title],
+                      threshold: 0.25
+                    }
                   }
   
-  belongs_to :user
+  
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true, length: { minimum: 50, maximum: 5000 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_210350) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_033533) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
