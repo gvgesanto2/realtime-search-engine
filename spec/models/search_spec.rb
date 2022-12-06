baf10223a1346e4428293d0a19d56fcb0c6c4082
Gabriel Santo <gvgesanto2@gmail.com>
2022-12-05 22:08:31 -0300

    test: Add unit tests for the Search model

---
+++ spec/models/search_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Search, type: :model do
+  subject do
+    fake_user = User.new(
+      name: 'Mr Test',
+      email: 'test@test.com',
+      password: '123123'
+    )
+
+    Search.new(
+      query: "hello world",
+      user: fake_user
+    )
+  end
+
+  before { subject.save }
+
+  it 'should be valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'should NOT be valid without a query' do
+    subject.query = nil
+    expect(subject).to_not be_valid
+  end
+end
