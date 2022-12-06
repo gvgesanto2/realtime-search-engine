27e95f16b580b3c1d16c8ffe9a0463623e5ad457
Gabriel Santo <gvgesanto2@gmail.com>
2022-12-05 22:04:46 -0300

    test: Add unit tests for the Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,5 +9,5 @@ class Article < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 100 }
-  validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :body, presence: true, length: { minimum: 50, maximum: 5000 }
 end
